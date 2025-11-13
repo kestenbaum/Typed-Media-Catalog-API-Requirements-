bf290fb2c0c827ba06380fc4da105bf9b29490a5
Oleksii <kespa71for@gmail.com>
2025-11-13 09:33:07 +0100

    feat: add AbstractRepository

--- src/app.ts
+++ src/app.ts
@@ -9,9 +9,37 @@ const app = express();
 const PORT = process.env.PORT || 3001
 
 app.use(express.urlencoded({ extended: true }));
-app.get("/", (req: Request, res: Response) => {
-    res.send("test")
-})
+
+const movieRepository = new MovieRepository();
+
+app.route('/movies')
+    .get((req: Request, res: Response) => {
+        try {
+            const movies = movieRepository.getAll();
+            return res.status(200).json(movies);
+        } catch (error) {
+            console.error('Error retrieving movies:', error);
+            return res.status(500).json({ message: 'Internal Server Error' });
+        }
+    })
+    .post((req: Request, res: Response) => {
+        const payload = req.body as Omit<MovieModel, 'id'>;
+
+        try {
+            const newMovie = movieRepository.create(payload);
+
+            return res.status(201).json(newMovie);
+        } catch (error) {
+            const errorMessage = (error as Error).message;
+            console.error(`Validation failed for movie creation: ${errorMessage}`);
+
+            return res.status(400).json({ 
+                error: 'Validation Failed', 
+                message: errorMessage 
+            });
+        }
+    });
+
 
 app.listen(PORT, () => {
     console.log(`Server listen port: ${PORT}`)
